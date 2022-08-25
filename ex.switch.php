955ac9ad009ac90968f1266137b97296575dba1a
HENRIQUE MARQUES SALES WOLTER <henrique.mswolter@senacsp.edu.br>
2022-08-24 22:08:26 -0300

    aula de switch

---
+++ ex.switch.php
@@ -0,0 +1,50 @@
+<?php
+
+$diaSemana = 1;
+
+switch ($diaSemana){
+   
+    case 0;
+        echo "Segunda";
+        break; 
+    case 1;
+        echo "Terca";
+        break;
+    case 2;
+        echo "Quarta";
+        break;
+    case 3;
+        echo "Quinta";
+        break;
+    case 4;
+        echo "Sexta";
+        break;
+    case 5;
+        echo "Sabado";
+        break;
+    case 6;
+        echo "Domimgo";
+        break;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-$nome= 'Prof Bono';
 
-echo "Olá $nome!";
-//Comentario de linha
+for ( $i = 0 ; $i < 10 ; $i++){
+    
+    echo "Numero da linha $i <br>"; 
+}
+
+$i = 0;
+while($i < 10); {
+    echo "Numero da linha $i <br>";
+    $i++;
+}
+
+$i = 0;
+do{
+    echo "Numero da linha $i <br>";
+    $i++;
+}while ( $i < 10 );
+
+//FIM LOPPINGS
