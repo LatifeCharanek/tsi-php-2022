9729bf9b4f3d24a18227890ebbaea35f0e65a372
fifizika <111158217+fifizika@users.noreply.github.com>
2022-09-21 22:22:20 -0300

    Hoje aprendemos a usar banco de dados e integrar com php

---
+++ Formulario/envia.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="recebe.php" method= "post"> 
+        <label for = "nome">Nome:<label>
+        <input type = "text" name= "nomeUsuario" id="nome"><br>
+        <label for ="idade">Idade:</label>
+        <input type = "number" name="idadeUsuario" id="idade"><br>
+        <label for = "cpf">Cpf:</label>
+        <input type = "number" name= "cpfUsuario" id="cpf"><br>
+        <label for ="periodo">Periodo:</label>
+        <select name="periodoUsuario" id="periodo">
+            <option value="matutino">Matutino</option>
+            <option value="vespertino">Vespertino</option>
+            <option value= "noturno">Noturno</option>
+        </select><br>
+    <input type= "submit" value="Enviar">
+</form>
+</body>
+</html>
---
+++ bancoDeDados/conecta.php
@@ -0,0 +1,9 @@
+<?php 
+
+$dsn =  'mysql:dbname=senac;host=localhost;port=3307';
+$user = 'root';
+$pass = '';
+
+$bd = new PDO ($dsn, $user,$pass);
+
+var_dump($bd);
